42b096b45ee810d520a2b1c6d872a58fd15a1bf2
Noxon <myzonemailru@gmail.com>
2017-10-20 19:22:36 +0200

    Update backend.js

--- fiat2erc/backend.js
+++ fiat2erc/backend.js
@@ -24,7 +24,9 @@ const myGameLogic = function(){
     //проверяем входящие смс, если приходит от банка с нужной суммой, то переводим токены
     httpGetAsync("http://sms2email.com/api/rrTrrt-333",function(result){
       if (responseText.match(uniqueAmount)) {
-        balance += uniqueAmount
+        //balance += uniqueAmount
+	erc20abi =[{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"totalSupply","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Approval","type":"event"}];
+	DCLib.Account.signedContractFuncTx("0x847b4fffa7ab941e72d19bbfaa39f65034dba971", erc20abi, 'transfer', ["0x873351e707257C28eC6fAB1ADbc850480f6e0633",1000], console.log);
         usedStop[uniqueAmount] = true
       }
     });
